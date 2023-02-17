747c6c8208cb05d8531588dca405c2b5e72541d8
Matheus Coutinho <mcoutinhoescudeiro@gmail.com>
2023-02-16 21:19:01 -0300

    insert perfil light

--- script.js
+++ script.js
@@ -2,4 +2,15 @@ function toggleMode() {
     const html = document.documentElement
 
     html.classList.toggle('light')
+
+    //busca a imagem
+    const img = document.querySelector("#profile img")
+
+//altera a imagem de modo light para dark
+    if(html.classList.contains("light")){
+
+        img.setAttribute("src", "./assets/avatar-light.png")
+    }else{
+        img.setAttribute("src", "./assets/avatar.png")
+    }
 }
